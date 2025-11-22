b298ebffe94c3ee59291a485ba60e0f964674dec
Zaed Khan <zaedkhan2019@gmail.com>
2025-11-22 11:25:54 -0800

    updated __init__.py of rag_engine

--- rag_engine/__init__.py
+++ rag_engine/__init__.py
@@ -1,18 +1,17 @@
-# datasage/__init__.py
-
-# Only import what's commonly used
-from datasage.indexing.index_engine import IndexingEngine
-from datasage.indexing.embedder import Embedder
-from datasage.indexing.vector_store import VectorStore
-from datasage.ingestion.chunker import TextChunker
-
-# Don't import generator/retriever at package level to avoid dependency issues
-# Users can import them directly when needed:
-# from datasage.query.generator import LLMGenerator
+from indexing.index_engine import IndexingEngine
+from indexing.embedder import Embedder
+from indexing.vector_store import VectorStore
+from ingestion.chunker import TextChunker 
+from ingestion.loaders import DocumentLoader
+from ingestion.loaders import PDFLoader
+from ingestion.loaders import CSVLoader
 
 __all__ = [
     "IndexingEngine",
     "Embedder", 
     "VectorStore",
     "TextChunker",
+    "DocumentLoader",
+    "PDFLoader",
+    "CSVLoader"
 ]
